dd5e52dccf33ae6248d0941b45442becc2756390
Wajdi Ballout <wajdi.ballout@blsk.dev>
2026-02-11 08:47:42 +0200

    security: guard forbidden prototype keys in object helper

--- src/lib/object.ts
+++ src/lib/object.ts
@@ -1,3 +1,9 @@
+const FORBIDDEN_KEYS = new Set(["__proto__", "constructor", "prototype"]);
+
 export const getObjectProperty = (value: object, key: string): unknown => {
+  if (FORBIDDEN_KEYS.has(key)) {
+    return undefined;
+  }
+
   return Reflect.get(value, key);
 };
